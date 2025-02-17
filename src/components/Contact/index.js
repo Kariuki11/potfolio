1b32479d619a8db5d954eeeaa7e493a9899eaaa6
Kariuki 11 <irungukenneth433@gmail.com>
2025-02-17 23:23:01 +0300

    Restore and implement Contact component with animated letters and loader

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -127,33 +127,33 @@ export default Contact;
 
 
 
-// import Loader from 'react-loaders';
-// import './index.scss'
-// import AnimatedLetters from '../AnimatedLetters';
-// import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
 
-// const Contact = () => {
-//         const [LetterClass, setLetterClass] = useState('text-animate')
+const Contact = () => {
+        const [LetterClass, setLetterClass] = useState('text-animate')
 
-//         useEffect(() => {
-//             return setTimeout(() => {
-//                 setLetterClass('text-animate-hover');
-//             }, 3000)
-//         },[])
+        useEffect(() => {
+            return setTimeout(() => {
+                setLetterClass('text-animate-hover');
+            }, 3000)
+        },[])
 
-//     return (<>
-//     <div className='container contact-page'>
-//         <div className='text-zone'>
-//             <h1>
-//                 <AnimatedLetters strArray={['C', 'o', 'n', 't', 'a','c', 't', ' ', 'M', 'e']}
-//                 idx={15}
-//                 />
-//             </h1>
-//         </div>
-//     </div>
-//     <Loader type="pacman" />
-//     </>)
-// }
+    return (<>
+    <div className='container contact-page'>
+        <div className='text-zone'>
+            <h1>
+                <AnimatedLetters strArray={['C', 'o', 'n', 't', 'a','c', 't', ' ', 'M', 'e']}
+                idx={15}
+                />
+            </h1>
+        </div>
+    </div>
+    <Loader type="pacman" />
+    </>)
+}
 
-// export default Contact;
+export default Contact;
 
