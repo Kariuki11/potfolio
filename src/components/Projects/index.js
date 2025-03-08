aec4b2ae99248d872fe2ea2225f7d8434af9ee06
Kariuki11 <irungukenneth433@gmail.com>
2025-03-08 23:57:00 +0300

    Updates on the prohjects section.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa'; // Import GitHub icon from react-icons
 import './index.scss'; // Import the SCSS file
+import LogoS from '../../assets/images/project2.png';
+import image from '../../assets/images/Airbnb.jpg';
+import image3 from '../../assets/images/Project3Img.png';
+import image4 from '../../assets/images/project4.png';
+import image5 from '../../assets/images/Project5.png';
+
 
 const projects = [
   {
@@ -8,7 +14,8 @@ const projects = [
     description: 'Web-based platform that allows users to search, book, and manage car rentals from various providers.',
     tags: ['#mongodo', '#tokwini'],
     githubUrl: 'https://github.com/yourusername/car-rent',
-    imageUrl: 'path/to/car-rent.jpg' // Replace with actual image path
+    // imageUrl: 'path/to/car-rent.jpg' // Replace with actual image path
+    image: "image",
   },
   {
     title: 'Job IT',
@@ -28,12 +35,14 @@ const projects = [
         title: 'Car Rent',
         description: 'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
         image: 'image1',
+        githubUrl: 'https://github.com/yourusername/car-rent',
         tags: ['#mongodo', '#tokwini'],
       },
       {
         title: 'Job IT',
         description: 'Web application that enables users to search for job openings, view estimated salary request for positions, and locate available jobs based on their current location.',
         image: 'image2',
+        githubUrl: 'https://github.com/yourusername/car-rent',
         tags: ['#rustap', '#kau'],
       },
       {
@@ -41,24 +50,73 @@ const projects = [
         description: 'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, can offer curated recommendations for popular destinations.',
         image: 'image3',
         tags: ['#rustj', '#supotone', '#cui'],
+        githubUrl: 'https://github.com/Kariuki11',
       },
       {
         title: 'AirBnB Platform clone',
         description: 'Web-based platform that allows users to search, book, and manage Home rentals from various providers, providing a convenient and efficient solution for Hospitality needs.',
         image: 'image4',
         tags: ['RestApi', 'Flask', 'Python', 'JavaScript'],
+        githubUrl: 'https://github.com/Kariuki11',
       },
       {
         title: 'Notifey',
         description: 'A Web Application that allow users to Organize their schedules including meetings and plans of the day. Users can create, Update and delete what is unnecessary to them.',
         image: 'image5',
         tags: ['ReactJS', 'Django', 'RestApi', 'Python'],
+        githubUrl: 'https://github.com/Kariuki11',
       },
       {
         title: 'Campoprime',
         description: 'A user friendly web Application where Student can Interact and share learnning materials among themselves. Educators can also sell thir courses to Students.',
         image: 'image6',
         tags: ['ReactJS', 'SupaBase', 'Tailwindcss'],
+        githubUrl: 'https://github.com/Kariuki11',
+      },
+      {
+        title: 'Notifey',
+        description: 'A Web Application that allow users to Organize their schedules including meetings and plans of the day. Users can create, Update and delete what is unnecessary to them.',
+        image: 'image5',
+        tags: ['ReactJS', 'Django', 'RestApi', 'Python'],
+        githubUrl: 'https://github.com/Kariuki11',
+      },
+      {
+        title: 'Campoprime',
+        description: 'A user friendly web Application where Student can Interact and share learnning materials among themselves. Educators can also sell thir courses to Students.',
+        image: 'image6',
+        tags: ['ReactJS', 'SupaBase', 'Tailwindcss'],
+        githubUrl: 'https://github.com/Kariuki11',
+      },
+      {
+        title: 'Notifey',
+        description: 'A Web Application that allow users to Organize their schedules including meetings and plans of the day. Users can create, Update and delete what is unnecessary to them.',
+        image: 'image5',
+        tags: ['ReactJS', 'Django', 'RestApi', 'Python'],
+        githubUrl: 'https://github.com/Kariuki11',
+      },
+      {
+        title: 'Campoprime',
+        description: 'A user friendly web Application where Student can Interact and share learnning materials among themselves. Educators can also sell thir courses to Students.',
+        // image: 'image6',
+        image: 'LogoS',
+        tags: ['ReactJS', 'SupaBase', 'Tailwindcss'],
+        githubUrl: 'https://github.com/Kariuki11',
+      },
+      {
+        title: 'Campoprime',
+        description: 'A user friendly web Application where Student can Interact and share learnning materials among themselves. Educators can also sell thir courses to Students.',
+        image: 'image6',
+        image: LogoS,
+        tags: ['ReactJS', 'SupaBase', 'Tailwindcss'],
+        githubUrl: 'https://github.com/Kariuki11',
+      },
+      {
+        title: 'Campoprime',
+        description: 'A user friendly web Application where Student can Interact and share learnning materials among themselves. Educators can also sell thir courses to Students.',
+        image: 'image6',
+        image: LogoS,
+        tags: ['ReactJS', 'SupaBase', 'Tailwindcss'],
+        githubUrl: 'https://github.com/Kariuki11',
       },
 ];
 
@@ -100,126 +158,6 @@ export default Projects;
 
 
 
-
-// import React from 'react';
-// import './index.scss'; // Import the SCSS file for styling
-
-// const projects = [
-//   {
-//     title: 'Car Rent',
-//     description: 'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
-//     image: 'image1',
-//     tags: ['#mongodo', '#tokwini'],
-//   },
-//   {
-//     title: 'Job IT',
-//     description: 'Web application that enables users to search for job openings, view estimated salary request for positions, and locate available jobs based on their current location.',
-//     image: 'image2',
-//     tags: ['#rustap', '#kau'],
-//   },
-//   {
-//     title: 'Trip Guide',
-//     description: 'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, can offer curated recommendations for popular destinations.',
-//     image: 'image3',
-//     tags: ['#rustj', '#supotone', '#cui'],
-//   },
-//   {
-//     title: 'AirBnB Platform clone',
-//     description: 'Web-based platform that allows users to search, book, and manage Home rentals from various providers, providing a convenient and efficient solution for Hospitality needs.',
-//     image: 'image4',
-//     tags: ['RestApi', 'Flask', 'Python', 'JavaScript'],
-//   },
-//   {
-//     title: 'Notifey',
-//     description: 'A Web Application that allow users to Organize their schedules including meetings and plans of the day. Users can create, Update and delete what is unnecessary to them.',
-//     image: 'image5',
-//     tags: ['ReactJS', 'Django', 'RestApi', 'Python'],
-//   },
-//   {
-//     title: 'Campoprime',
-//     description: 'A user friendly web Application where Student can Interact and share learnning materials among themselves. Educators can also sell thir courses to Students.',
-//     image: 'image6',
-//     tags: ['ReactJS', 'SupaBase', 'Tailwindcss'],
-//   },
-//   {
-//     title: 'Django Project',
-//     description: 'A user friendly marketplace platform that allows admins or sellers to easily list, manage, and sell their products online. The platform enables sellers to add goods with detailed descriptions, images, and pricing.',
-//     image: 'image7',
-//     tags: ['Python', 'Django', 'PostgreSQL'],
-//   },
-//   {
-//     title: 'IoT Project',
-//     description: 'An IoT-based automated greenhouse system designed to monitor and manage environmental factors. The system includes features such as temperature and humidity detection, pH monitoring, and an SMS notification system that alerts users with real-time updates directly to their phones. It utilizes ThinkSpeak for data visualization and AWS Cloud for reliable data storage, ensuring efficient and scalable performance.',
-//     image: 'image8',
-//     tags: ['Esp32s', 'Arduino', 'ThinkSpeak', 'AWS Cloud', 'SMS Notification'],
-//   },
-//   {
-//     title: 'Project 9',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-//     image: 'image9',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-//   {
-//     title: 'Project 10',
-//     description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-//     image: 'image10',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-//   {
-//     title: 'Project 11',
-//     description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-//     image: 'image11',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-//   {
-//     title: 'Project 12',
-//     description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-//     image: 'image12',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-//   {
-//     title: 'Project 13',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-//     image: 'image13',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-//   {
-//     title: 'Project 14',
-//     description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-//     image: 'image14',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-//   {
-//     title: 'Project 15',
-//     description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-//     image: 'image15',
-//     tags: ['Tag1', 'Tag2', 'Tag3'],
-//   },
-// ];
-
-// const ProjectsSection = () => {
-//   return (
-//     <div className="projects-container">
-//       <h1>My Projects</h1>
-//       <div className="projects-grid">
-//         {projects.map((project, index) => (
-//           <div key={index} className="project-card">
-//             <img src={project.image} alt={project.title} className="project-image" />
-//             <h3 className="project-title">{project.title}</h3>
-//             <p className="project-description">{project.description}</p>
-//             <div className="project-tags">
-//               {project.tags.map((tag, idx) => (
-//                 <span key={idx} className="tag">{tag}</span>
-//               ))}
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProjectsSection;
 
 
 
